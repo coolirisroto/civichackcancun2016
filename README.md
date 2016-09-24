31dcd9c780be20590080ee73bebf8f83c8e13aef
germanpunk <german@punkmkt.com>
2016-09-24 15:24:56 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# civichackcancun2016
---
+++ hackaton/settings/production.py
@@ -0,0 +1,17 @@
+from base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_secret('DATABASE_NAME'),
+        'USER': get_secret('DATABASE_USER'),
+        'PASSWORD': get_secret('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
